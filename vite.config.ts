534d256ee54ad6cb87efdd0fabf064b6cb639fd1
alexander-entin <alexander.entin@gmail.com>
2023-05-24 15:45:55 +0300

    netlify edge

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    rakkas({ adapter: "netlify" }),
+    rakkas({ adapter: "netlify-edge" }),
   ],
 });
